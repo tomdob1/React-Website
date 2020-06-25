bb6ce22b5fbcc38a191b0ecb8a0c2776b788bfe8
tomdob1 <tomasz_dob@msn.com>
2020-06-25 14:38:28 +0100

    Made more dynamic

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './BusinessList.css';
+
 import Business from '../Business/Business'; // imports business component from Business.js
 
 class BusinessList extends React.Component {
     render(){
-
         return(
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                
+                {
+                    this.props.businesses.map( business => {
+                       return <Business business={business}/>
+                    })   
+                } 
             </div>
         );
     }
-};
+}
 
 export default BusinessList;
